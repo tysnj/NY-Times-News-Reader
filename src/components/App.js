e94bbb8ee4ca2c8fafefb27f694be775a1e19d41
Tyson McNutt <tyson.mcnutt@gmail.com>
2021-08-03 22:09:53 -0600

    Add NavBar component

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+
+
+const App = () => {
+
+  return (
+
+  );
+}
+
+export default App;
