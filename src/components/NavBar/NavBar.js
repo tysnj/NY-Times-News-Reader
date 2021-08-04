55667df4e2b05a8146ab76e2bcd67dbaeb4a298e
Tyson McNutt <tyson.mcnutt@gmail.com>
2021-08-04 10:02:47 -0600

    Add NavBar styled elements

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaTimes, FaBars } from 'react-icons/fa';
+import { IconContext } from 'react-icons/lib';
+import {
+  Nav,
+  NavBarContainer,
+  NavLogo,
+  NavIcon,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLink
+} from './NavBarElements'
 
 const NavBar = () => {
+  const [click, setClick] = useState(false)
+  
+  const handleClick = () => setClick(!click)
+
   return (
-    <div>
-      
-    </div>
+    <IconContext.Provider value={{color: '#191919'}}>
+      <Nav>
+        <NavBarContainer>
+          <NavLogo>
+            <NavIcon/>
+              NY Times News Reader
+          </NavLogo>
+          <MobileIcon onClick={handleClick}>
+            {click ? <FaTimes/> : <FaBars/>}
+          </MobileIcon>
+          <NavMenu onClick={handleClick} click={click}>
+            <NavItem>
+              <NavLink to='/home'>
+                Home
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to='/categories'>
+                Categories
+              </NavLink>
+            </NavItem>
+          </NavMenu>
+        </NavBarContainer>
+      </Nav>
+    </IconContext.Provider>
   )
 }
 
--- src/components/NavBar/NavBarElements.js
+++ src/components/NavBar/NavBarElements.js
@@ -1,1 +1,105 @@
 import styled from 'styled-components';
+import { Container } from '../../globalStyles';
+import { Link } from 'react-router-dom'
+import { GiWorld } from 'react-icons/gi'
+
+
+export const Nav = styled.nav`
+  background: #91B6FB;
+  height: 80px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 2rem;
+  position: sticky;
+  top: 0;
+  z-index: 999;
+`;
+
+export const NavBarContainer = styled(Container)`
+  display: flex;
+  justify-content: space-between;
+  height: 80px;
+  ${Container}
+`
+
+export const NavLogo = styled(Link)`
+  color: #191919;
+  justify-self: flex-start;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 1.3rem;
+  display: flex;
+  align-items: center;
+`;
+
+
+export const NavIcon = styled(GiWorld)`
+  margin-left: 0.2rem;
+  margin-right: 0.5rem;
+`
+
+export const MobileIcon = styled.span`
+  display: none;
+  @media screen and (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 60%);
+    font-size: 1.8rem;
+    cursor: pointer;
+  }
+`
+
+export const NavMenu = styled.ul`
+  display: flex;
+  align-items: center;
+  list-style: none;
+  text-align: center;
+  @media screen and (max-width: 768px) {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 90vh;
+    position: absolute;
+    top: 80px;
+    left: ${({click}) => (click ? 0 : '-100%')};
+    opacity: 1;
+    transition: all 0.5s ease;
+    background: #91B6FB;
+  }`
+
+export const NavItem = styled.li`
+  height: 80px;
+  border-bottom: 2px solid transparent;
+  &:hover {
+    border-bottom: 2px solid #4b59f7;
+  }
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    &:hover {
+      border: none;
+    }
+  }
+`
+
+export const NavLink = styled(Link)`
+  color: #191919;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  padding: 0.5rem 1rem;
+  height: 100%;
+  @media screen and (max-width: 768px) {
+    text-align: center;
+    padding: 2rem;
+    width: 100%;
+    display: table;
+    &:hover {
+      color: #fff;
+      transition: all 0.3s ease;
+    }
+  }
+`;
